4c1076961c42e6e995615e99bc114b7292b6c181
Chloe Donegan <chloedonegan@gmail.com>
2016-03-15 19:24:41 +0000

    remove jquery

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var templateString = $('.template').text();
+var templateString = document.querySelector('.template').textContent;
 
 var template = _.template(templateString);
 
@@ -37,8 +37,32 @@ function setState(newState) {
 }
 
 function render() {
-  $('.chow__content').html(template(state));
+  document.querySelector('.chow__content').innerHTML = template(state);
+  bindEvents();
 }
+
+function bindEvents() {
+  var options = document.querySelectorAll('.chow__options li');
+
+  _.each(options, function(option, idx) {
+
+    option.addEventListener('click', function(event) {
+      var searchTerm = this.getAttribute('data-option-name');
+
+      var activeOption = _.find(state.options, function (option) {
+        return option.searchTerm === searchTerm;
+      });
+
+      console.log(this);
+      this.classList.add('active');
+
+      setState({activeOption: activeOption});
+      getVenues();
+    });
+
+  });
+}
+
 
 function getVenues() {
 
@@ -49,72 +73,76 @@ function getVenues() {
 
   var formatedLocation = encodeURIComponent(state.location);
 
+  var url = "https://api.foursquare.com/v2/venues/explore?client_id=YQ3TW4N1D2L5I4X2GR5W53AJNQJY5OD02IWWO5XD3LLDH0IJ&client_secret=EPVJCXN4CJO2CRU02WY5A3IUQ1T0SNHVIHV01JRYCQ4IKYLY&v=20140806&query="+state.activeOption.searchTerm+"&radius=1000&near="+formatedLocation+"%2C%20London%2C%20Greater%20London";
+
   setState({
     loading: true,
     venues: []
   });
 
-  $.ajax({
-    url: "https://api.foursquare.com/v2/venues/explore?client_id=YQ3TW4N1D2L5I4X2GR5W53AJNQJY5OD02IWWO5XD3LLDH0IJ&client_secret=EPVJCXN4CJO2CRU02WY5A3IUQ1T0SNHVIHV01JRYCQ4IKYLY&v=20140806&query="+state.activeOption.searchTerm+"&radius=1000&near="+formatedLocation+"%2C%20London%2C%20Greater%20London",
-    dataType: 'json'
-  }).done(function(data) {
+  fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
 
-    var responseVenues = data.response.groups[0].items;
-    var venues = [];
+        var responseVenues = data.response.groups[0].items;
+        var venues = [];
 
-    if (responseVenues && responseVenues.length > 0) {
+        if (responseVenues && responseVenues.length > 0) {
 
-      _.each(responseVenues, function (responseVenue) {
+          _.each(responseVenues, function (responseVenue) {
 
-        if (!responseVenue.venue.price || !responseVenue.venue.rating) {
-          return;
+            if (!responseVenue.venue.price || !responseVenue.venue.rating) {
+              return;
+            }
+
+            var venue = {
+                name: responseVenue.venue.name,
+                url: responseVenue.venue.url || '',
+                rating: responseVenue.venue.rating || ''
+            };
+
+
+            if (responseVenue.venue.price && responseVenue.venue.price.message) {
+              venue.priceBracket = responseVenue.venue.price.message;
+            }
+
+            if (responseVenue.venue.hours && responseVenue.venue.hours.status) {
+              venue.openStatus = responseVenue.venue.hours.status;
+            }
+
+            if (responseVenue.venue.menu && responseVenue.venue.menu.url) {
+              venue.menu = responseVenue.venue.menu.url;
+            }
+
+            venues.push(venue);
+
+          });
         }
 
-        var priceBracket = '';
-        if (responseVenue.venue.price && responseVenue.venue.price.message) {
-          priceBracket = responseVenue.venue.price.message;
-        }
-
-        var openStatus = '';
-        if (responseVenue.venue.hours && responseVenue.venue.hours.status) {
-          openStatus = responseVenue.venue.hours.status;
-        }
-
-        var menu = '';
-        if (responseVenue.venue.menu && responseVenue.venue.menu.url) {
-          menu = responseVenue.venue.menu.url;
-        }
-
-        venues.push({
-            name: responseVenue.venue.name,
-            url: responseVenue.venue.url,
-            rating: responseVenue.venue.rating,
-            priceBracket: priceBracket,
-            openStatus: openStatus,
-            menu: menu
+        var sortedVenues = venues.sort(function(a, b) {
+          return b.rating - a.rating;
         });
 
-      });
-    }
+        setState({
+          loading: false,
+          venues: sortedVenues
+        });
 
-    var sortedVenues = venues.sort(function(a, b) {
-      return b.rating - a.rating;
-    });
-
-    setState({
-      loading: false,
-      venues: sortedVenues
-    });
-
-  }).fail(function() {
+  }).catch(function() {
     console.log("something went wrong");
   });
 }
 
+document.querySelector('.chow__clear-button').addEventListener('click', function(e) {
+  setState(initialState);
+});
 
-$(document).on('submit', '.chow__search', function (event) {
+
+document.querySelector('.chow__search').addEventListener('submit', function(event) {
   event.preventDefault();
-  var location = $(this).find('.chow__search-input').val();
+  var location = this.querySelector('.chow__search-input').value;
   console.log(location);
 
   setState({
@@ -123,23 +151,7 @@ $(document).on('submit', '.chow__search', function (event) {
   });
 
   console.log(state);
-});
 
-
-$(document).on('click', '.chow__options li', function(e) {
-  var searchTerm = $(this).data('option-name');
-
-  var activeOption = _.find(state.options, function(option) {
-    return option.searchTerm === searchTerm;
-  });
-
-  setState({ activeOption: activeOption });
-  getVenues();
-});
-
-
-$(document).on('click', '.chow__clear-button', function(e) {
-  setState(initialState);
 });
 
 
